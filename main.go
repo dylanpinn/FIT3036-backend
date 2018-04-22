fc2ca5403a4af2b48bba7dce2c46e71403c0deeb
Dylan Pinn <dylan@dylanpinn.com>
2018-04-22 21:24:25 +1000

    Start adding CalculateArea method

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	area := CalculateArea()
+	fmt.Printf("Calculated Area: %d", area)
+}
+
+// CalculateArea the area of ...
+func CalculateArea() int {
+	return 10
+}
