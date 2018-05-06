b05b38711a413310ecf0130b927cf382ebfc7db8
Dylan Pinn <dylan@dylanpinn.com>
2018-05-06 16:32:43 +1000

    Fix tests

--- area_test.go
+++ area_test.go
@@ -1,13 +1,18 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+const TOLLERANCE = 0.00001
 
 func TestCalculateDistance(t *testing.T) {
 	pt1 := Coordinate{lat: -37.92, long: 145.16}
 	pt2 := Coordinate{lat: -37.92, long: 145.17}
 	distance := CalculateDistance(pt1, pt2)
 
-	if distance != 0.877183 {
+	if diff := math.Abs(distance - 0.877183); diff > TOLLERANCE {
 		t.Errorf("Distance was incorrect, got: %f, want: %f.", distance, 0.877183)
 	}
 }
