150917d664488395e713a5f269bb22031167ebc1
Dylan Pinn <dylan@dylanpinn.com>
2018-05-19 20:46:41 +1000

    Connect to HTTP event

--- remote/main.go
+++ remote/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func hello() (string, error) {
-	return "Hello ƛ!", nil
+func handleRequest(ctx context.Context,
+	request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Processing request data for request %s.\n",
+		request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+
+	fmt.Println("Headers:")
+	for key, value := range request.Headers {
+		fmt.Printf("    %s: %s\n", key, value)
+	}
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
-	lambda.Start(hello)
+	lambda.Start(handleRequest)
 }
